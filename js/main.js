32a10d8cc30964cbb5ace9fce1b5941dff958f8f
Heiko-Lemmert <heiko.lemmert91@gmail.com>
2024-08-27 17:24:27 +0200

    optimized sidebar

--- js/main.js
+++ js/main.js
@@ -6,53 +6,61 @@ function includeHTML() {
     /* Loop through a collection of all HTML elements: */
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
         }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
-      }
     }
-  }
+    checkHTMLforClassList();
+}
 
 function init() {
     includeHTML();
-    setTimeout(sidebarClicked, 1500);
 }
 
-  // Sidebar JS
-  function sidebarClicked() {
-    // Array mit den IDs der Elemente
-    let ids = ['summary', 'addTask', 'board', 'contacts','privacyPolicy','legalNotice'];
-
-    // Schleife durch die IDs der Menüelemente
-    for (let i = 0; i < ids.length; i++) {
-        // Wähle das Element anhand seiner ID aus
-        let element = document.getElementById(ids[i]);
-
-        // Weise dem Element einen Klick-Eventlistener zu
-        element.onclick = function() {
-            // Entferne die "clicked"-Klasse von allen Elementen
-            for (let j = 0; j < ids.length; j++) {
-                document.getElementById(ids[j]).classList.remove('clicked');
-            }
-
-            // Füge die "clicked"-Klasse nur dem geklickten Element hinzu
-            this.classList.add('clicked');
-        };
+// Sidebar JS
+function checkHTMLforClassList() {
+    const currentHTML = window.location.pathname;
+    switch (currentHTML) {
+        case '/summary.html':
+            addClassList('summary')
+            break;
+        case '/add-task.html':
+            addClassList('addTask')
+            break;
+        case '/board.html':
+            addClassList('board')
+            break;
+        case '/contacts.html':
+            addClassList('contacts')
+            break;
+        case '/privacy-policy.html':
+            addClassList('privacyPolicy')
+            break;
+        case '/legal-notice.html':
+            addClassList('legalNotice')
+            break;
+        default:
+            break;
     }
-  }
+}
+
+function addClassList(id) {
+    return document.getElementById(id).classList.add('clicked');
+}
