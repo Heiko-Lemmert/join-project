12a14841d581d7cdba90d8390f66cdf551fb15df
Heiko-Lemmert <heiko.lemmert91@gmail.com>
2024-09-05 19:12:53 +0200

    Add Task HTML Design Functions finish

--- js/add-task.js
+++ js/add-task.js
@@ -6,14 +6,33 @@ const contacts = [
     { name: 'Jane Smith', initials: 'JS' },
 ];
 
+let contact = [];
+
+const taskTitle = document.getElementById('taskTitle');
+const taskDescription = document.getElementById('taskDescription');
+const taskDate = document.getElementById('taskDate');
 const contactSelector = document.getElementById('contactSelector');
+const taskSelector = document.getElementById('taskSelector');
 const selectedContacts = document.getElementById('selectedContacts');
+const contactSearch = document.getElementById('contactSearch');
+const enhanceBtn = document.getElementById('enhanceBtn');
+const addSubtask = document.getElementById('addSubtask');
+const subtask = document.getElementById('subtask');
+const addBtn = document.getElementById('addBtn');
 const contactBackgroundColor = ['#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8', '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701', '#0038FF', '#0038FF', '#FFE62B', '#FF4646', '#FFBB2B'];
+const dotMarker = '&bull;';
+let writtenSubtask = [];
+let selected = [];
 
 function init() {
     includeHTML();
     renderTaskContact();
     attachEventListeners(); // Event-Listener erst nach dem Rendern der Kontakte hinzufügen
+    contact = getData('contacts');
+    postData("contacts", contacts)
+    .catch(error => {
+        console.error("Error posting data:", error);
+    });
 }
 
 // Funktion zur Generierung einer zufälligen Farbe
@@ -28,32 +47,83 @@ function renderTaskContact() {
         <div class="contact-option">
             <div class="contact-initials" style="background-color: ${getRandomColor()}">${contact.initials}</div>
             <label>${contact.name}</label>
-            <input type="checkbox" name="contact" value="${contact.initials.toLowerCase()}">
+            <img src="./assets/img/no-check-btn.png" alt="Check" class="check-icon" id="checkIcon">
         </div>`;
     });
 }
 
-// Erstellt Click events nach dem Rendern.
+// Erstellt Click-Events nach dem Rendern.
 function attachEventListeners() {
+    contactEventLister();
+    taskEventlister();
+    subtaskEventlister();
+}
+
+function contactEventLister() {
     const contactOptions = document.getElementById('contactOptions');
     const contactOptionsDivs = contactOptions.querySelectorAll('.contact-option');
-    
+
     contactOptionsDivs.forEach(optionDiv => {
         optionDiv.addEventListener('click', () => {
-            const checkbox = optionDiv.querySelector('input[type="checkbox"]');
-            checkbox.checked = !checkbox.checked; // Toggle checkbox status
+            checkOptionDiv(optionDiv);
             updateSelectedContacts(); // Update selected contacts
         });
-        optionDiv.querySelector('input[type="checkbox"]').addEventListener('change', () => {
-            updateSelectedContacts();
-        })
     });
 
     contactSelector.addEventListener('click', () => {
+        contactSelector.classList.toggle('field-activ');
+        contactSelector.querySelector('img').classList.toggle('arrow-rotate');
         contactOptions.style.display = contactOptions.style.display === 'block' ? 'none' : 'block';
-        contactSelector.style.border = contactSelector.style.border === '1px solid rgb(144, 209, 237)' ? '1px solid #ccc;' : '1px solid #90D1ED';
-
+        contactSearch.style.display = contactSearch.style.display === 'block' ? 'none' : 'block';
     });
+};
+
+function checkOptionDiv(optionDiv) {
+    const checkIcon = optionDiv.querySelector('.check-icon');
+    const isSelected = checkIcon.getAttribute('src') === './assets/img/check-btn.png';
+    checkIcon.setAttribute('src', isSelected ? './assets/img/no-check-btn.png' : './assets/img/check-btn.png');
+    optionDiv.classList.toggle('sc-check');
+}
+
+function taskEventlister() {
+    const taskOptions = document.getElementById('taskOptions');
+    const taskOptionsDivs = taskOptions.querySelectorAll('.task-option');
+
+    taskSelector.addEventListener('click', () => {
+        taskOptions.style.display = taskOptions.style.display === 'block' ? 'none' : 'block';
+        if (taskSelector.innerText === 'Select task category') {
+            taskSelector.querySelector('img').classList.toggle('arrow-rotate');
+        }
+    })
+
+    taskOptionsDivs.forEach(taskDiv => {
+        taskDiv.addEventListener('click', event => {
+            console.log(event.target.innerText);
+            taskSelector.textContent = '';
+            taskSelector.textContent = event.target.innerText;
+        })
+    })
+}
+
+
+function updateSelectedContacts() {
+    const contactOptions = document.getElementById('contactOptions');
+    selected = [];
+
+    contactOptions.querySelectorAll('.contact-option').forEach(optionDiv => {
+        const checkIcon = optionDiv.querySelector('.check-icon');
+        if (checkIcon.getAttribute('src') === './assets/img/check-btn.png') {
+            const textContent = optionDiv.querySelector('.contact-initials').textContent;
+            const getStyle = optionDiv.querySelector('.contact-initials').getAttribute('style');
+            selected.push(`<div class="contact-initials" style="${getStyle}">${textContent}</div>`);
+        }
+    });
+
+    if (selected.length > 0) {
+        selectedContacts.innerHTML = selected.join('');
+    } else {
+        selectedContacts.textContent = '';
+    }
 }
 
 // Funktionen für die Priorität Button`s
@@ -75,7 +145,7 @@ function prioChooser(prio) {
 
 function checkedPrioBtn(id, prio) {
     resetPrioBtn();
-    document.getElementById(id).classList.add('checked-'+prio);
+    document.getElementById(id).classList.add('checked-' + prio);
 }
 
 function resetPrioBtn() {
@@ -84,35 +154,128 @@ function resetPrioBtn() {
     document.getElementById('prioLow').classList.remove('checked-low');
 }
 
- /**
-//  * updateSelectedContacts: This function collects the names of the selected contacts and 
-//  * updates the display in the drop-down menu as well as the list of selected contacts.
-//  * AND
-//  * click-Eventlistener: Ensures that the drop-down menu is closed when the user clicks outside the menu.
-//  */
-function updateSelectedContacts() {
-    const contactOptions = document.getElementById('contactOptions');
-    const checkboxes = contactOptions.querySelectorAll('input[type="checkbox"]');
-    const selected = [];
+function subtaskEventlister() {
+    const addSubtaskQuery = addSubtask.querySelectorAll('img');
+    addSubtaskQuery.forEach(subElement => {
+        subElement.addEventListener('click', event => {
+            if (event.target.id === 'addBtn') {
+                enhanceBtn.style.display = 'flex';
+                addBtn.style.display = 'none';
+                addSubtask.classList.toggle('field-activ');
+                subtask.focus();
+            } else if (event.target.id === 'closeBtn') {
+                subtask.value = '';
+                enhanceBtn.style.display = 'none';
+                addBtn.style.display = 'block';
+                addSubtask.classList.toggle('field-activ');
+            } else {
+                if (subtask.value.length >= 1) {
+                    createSubtask(subtask.value);
+                }
+            }
+        })
+    })
+    addSubtask.querySelector('input').addEventListener('click', () => {
+        enhanceBtn.style.display = 'flex';
+        addBtn.style.display = 'none';
+        addSubtask.classList.toggle('field-activ');
+    })
+}
 
-    checkboxes.forEach(checkbox => {
-        if (checkbox.checked) {
-            selected.push(checkbox.parentElement.querySelector('label').textContent);
-        }
-    });
+function createSubtask(newSubtask) {
+    writtenSubtask.push(newSubtask);
+    subtask.value = '';
+    renderSubtask();
+}
 
-    if (selected.length > 0) {
-        selectedContacts.textContent = 'Selected: ' + selected.join(', ');
-        contactSelector.textContent = selected.join(', ');
-    } else {
-        selectedContacts.textContent = '';
-        contactSelector.textContent = 'Select contacts to assign';
+function deleteSubtask(id) {
+    writtenSubtask.splice(id, 1);
+    renderSubtask();
+}
+
+function editSubtask(id) {
+    const editTask = document.getElementById('edit-' + id);
+    const editTaskField = document.getElementById('editTaskField-' + id);
+    editTask.style.display = 'flex';
+    editTaskField.focus();
+}
+
+function editSubtaskArry(id) {
+    const editSubtaskField = document.getElementById('editTaskField-' + id);
+    const newSubtask = editSubtaskField.value;
+    writtenSubtask.splice(id, 1, newSubtask);
+    renderSubtask();
+}
+
+function renderSubtask() {
+    const showSubtask = document.getElementById('showSubtask');
+    showSubtask.innerHTML = '';
+    for (let i = 0; i < writtenSubtask.length; i++) {
+        const subtask = writtenSubtask[i];
+        showSubtask.innerHTML += `
+            <div class="new-subtask">
+                <p>${dotMarker} ${subtask}</p>
+                <div class="new-subtask-btn">
+                    <img src="./assets/img/edit-subtask.png" onclick="editSubtask(${i})" alt="Edit">
+                    <img src="./assets/img/delete-subtask.png" onclick="deleteSubtask(${i})" alt="Delete">
+                </div>
+                <div class="edit-field" id="edit-${i}">
+                    <input name="" value="${subtask}" id="editTaskField-${i}">
+                    <div class="edit-field-btn">
+                        <img src="./assets/img/delete-subtask.png" onclick="deleteSubtask(${i})" alt="Delete">
+                        <hr>
+                        <img src="./assets/img/check-addtask.png" alt="Enter" onclick="editSubtaskArry(${i})">
+                    </div>
+                </div>
+            </div>`;
     }
+}
+
+function clearTask(event) {
+    const contactOptions = document.getElementById('contactOptions');
+    const contactOptionsDivs = contactOptions.querySelectorAll('.sc-check');
+    event.preventDefault();
+    taskTitle.value = '';
+    taskDescription.value = '';
+    selected = [];
+    selectedContacts.textContent = '';
+    contactOptionsDivs.forEach(optionDiv => {
+        checkOptionDiv(optionDiv);
+    })
+    taskDate.value = '';
+    resetPrioBtn();
+    taskSelector.innerHTML = 'Select task category <img class="task-selector-arrow" src="./assets/img/arrow-down.png" alt="Arrow">';
+    writtenSubtask = [];
+    renderSubtask();
+}
+
+function createTask(event) {
+    event.preventDefault();
+    console.log('Hello World')
 }
 
 document.addEventListener('click', (event) => {
     const contactOptions = document.getElementById('contactOptions');
-    if (!contactSelector.contains(event.target) && !contactOptions.contains(event.target)) {
+    const taskOptions = document.getElementById('taskOptions');
+    if (!contactSelector.contains(event.target) && !contactOptions.contains(event.target) && !contactSearch.contains(event.target)) {
         contactOptions.style.display = 'none';
+        contactSearch.style.display = 'none';
+        if (contactSelector.classList.contains('field-activ')) {
+            contactSelector.classList.remove('field-activ');
+            contactSelector.querySelector('img').classList.toggle('arrow-rotate');
+        }
+    }
+    if (!taskSelector.contains(event.target)) {
+        taskOptions.style.display = 'none';
+        if (taskOptions.innerText === 'Select task category') {
+            taskSelector.querySelector('img').classList.toggle('arrow-rotate');
+        }
+    }
+
+    if (!addSubtask.contains(event.target)) {
+        if (addSubtask.classList.contains('field-activ')) {
+            addSubtask.classList.remove('field-activ');
+            enhanceBtn.style.display = 'none';
+        }
     }
 });
