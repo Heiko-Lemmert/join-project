19b943b3f7557d1dace1774bbda6f495c4e494ec
Lars Siebert <149431364+larsbjj@users.noreply.github.com>
2024-10-02 11:57:47 +0200

    load contacts from firebase database

--- js/contacts.js
+++ js/contacts.js
@@ -1,12 +1,28 @@
+let contactArray = [];
+
 async function loadContacts() {
     const contacts = await getData("contacts");
+    contactArray = Object.values(contacts)[0]; 
 
-    if (contacts) {
-        console.log(contacts);
-        
-    } else {
-        console.log("Keine Kontakte gefunden");
-    }    
+    renderContacts();
 }
 
-loadContacts();
+function renderContacts() {
+    let contactSection = document.getElementById('contact-section');
+    contactSection.innerHTML = '';
+
+    for (let index = 0; index < contactArray.length; index++) {
+        contactSection.innerHTML += contactItemTemplate(index);
+        
+    }
+}
+
+function contactItemTemplate(index) {
+    return `
+    <div class="contact-item">
+                <span class="contact-initial">SB</span>
+                <div class="contact-info">
+                    <span class="contact-name">${contactArray[index].name}</span>
+                    <span class="contact-email">${contactArray[index].email}</span>
+                </div>`
+}
