98cecd06f9bcd2e9d9fe1e3cd380be41c093e0aa
Lars Siebert <149431364+larsbjj@users.noreply.github.com>
2024-10-02 14:14:57 +0200

    clicking on a contact, displays their information

--- js/contacts.js
+++ js/contacts.js
@@ -2,7 +2,9 @@ let contactArray = [];
 
 async function loadContacts() {
     const contacts = await getData("contacts");
-    contactArray = Object.values(contacts)[0]; 
+    contactArray = Object.values(contacts)[0];
+    console.log(contactArray);
+     
 
     renderContacts();
 }
@@ -18,11 +20,37 @@ function renderContacts() {
 }
 
 function contactItemTemplate(index) {
-    return `
-    <div class="contact-item">
-                <span class="contact-initial">SB</span>
-                <div class="contact-info">
-                    <span class="contact-name">${contactArray[index].name}</span>
-                    <span class="contact-email">${contactArray[index].email}</span>
-                </div>`
+    return `<div class="contact-item" onclick="showContactDetails(${index})">
+    <span class="contact-initial">SB</span>
+    <div class="contact-info">
+    <span class="contact-name">${contactArray[index].name}</span>
+    <span class="contact-email">${contactArray[index].email}</span>
+    </div>
+    </div>`
+}
+
+function showContactDetails(index) {
+    let contactDetails = document.getElementById('contact-card');
+    contactDetails.innerHTML = '';
+    contactDetails.innerHTML = contactDetailsTemplate(index);
+}
+
+function contactDetailsTemplate(index) {
+    return `            <div class="profile-section">
+                <img class="profile-img">
+                <div class="contact-name">
+                    <h1 class="profile-name">${contactArray[index].name}</h1>
+                    <div class="profile-actions">
+                    <a href="#" class="profile-edit">Edit</a>
+                    <a href="#" class="profile-delete">Delete</a>
+                    </div>
+                </div>
+            </div>
+            <div class="contact-details">
+                <h2 class="details-heading">Contact Information</h2>
+                <p class="contact-label"><strong>Email</strong></p>
+                <a href="#" class="contact-email">${contactArray[index].email}</a>
+                <p class="contact-label"><strong>Phone</strong></p>
+                <a href="#" class="contact-phone">${contactArray[index].number}</a>
+            </div>`
 }
--- js/firebase.js
+++ js/firebase.js
@@ -34,3 +34,67 @@ async function updateData(path = "", data = {}) {
     });
     return responseToJson = await response.json(); 
 }
+
+async function postData(path = "", data = {}) {
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    return responseToJson = await response.json(); 
+}
+
+postData('contacts', [
+    {
+        "email": "max.mueller@example.com",
+        "name": "Max Müller",
+        "number": 15510123456
+    },
+    {
+        "email": "anna.schmidt@example.com",
+        "name": "Anna Schmidt",
+        "number": 15510234567
+    },
+    {
+        "email": "luca.meier@example.com",
+        "name": "Luca Meier",
+        "number": 15510345678
+    },
+    {
+        "email": "laura.schneider@example.com",
+        "name": "Laura Schneider",
+        "number": 15510456789
+    },
+    {
+        "email": "philipp.schulz@example.com",
+        "name": "Philipp Schulz",
+        "number": 15510567890
+    },
+    {
+        "email": "emilia.wagner@example.com",
+        "name": "Emilia Wagner",
+        "number": 15510678901
+    },
+    {
+        "email": "felix.hoffmann@example.com",
+        "name": "Felix Hoffmann",
+        "number": 15510789012
+    },
+    {
+        "email": "sofia.klein@example.com",
+        "name": "Sofia Klein",
+        "number": 15510890123
+    },
+    {
+        "email": "noah.fischer@example.com",
+        "name": "Noah Fischer",
+        "number": 15510901234
+    },
+    {
+        "email": "hanna.weber@example.com",
+        "name": "Hanna Weber",
+        "number": 15511012345
+    }
+]);
