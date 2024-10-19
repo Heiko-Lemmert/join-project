b161c5902d9c3a01a501f988e436bc5a50feddb2
Lars Siebert <149431364+larsbjj@users.noreply.github.com>
2024-10-19 15:04:54 +0200

    page content is only visible if a user is logged in, users can log out

--- js/auth.js
+++ js/auth.js
@@ -2,6 +2,7 @@ function checkUserLoggedIn() {
     const user = localStorage.getItem("currentUser"); 
     if (user) {
         console.log("User is logged in:", JSON.parse(user));
+        document.body.style.visibility = "visible";
     } else {
         console.log("No user is logged in.");
         window.location.href = "index.html";
@@ -12,4 +13,5 @@ function logout() {
     localStorage.removeItem("currentUser");
     console.log("User logged out.");
     window.location.href = "index.html";
+    document.body.style.visibility = "hidden";
 }
